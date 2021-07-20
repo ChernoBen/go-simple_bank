1cc3ffe11d14076a4e36090bf62c0aa812a2c632
ChernoBen <bneto@computacao.cua.ufmt.br>
2021-07-20 13:35:39 -0400

    refactoring

--- db/sqlc/store.go
+++ db/sqlc/store.go
@@ -37,3 +37,32 @@ func (store *Store) execTx(ctx context.Context, fn func(*Queries) error) error {
 	}
 	return tx.Commit()
 }
+
+type TransferTxParams struct {
+	FromAccountID int64 `json:"from_account_id"`
+	ToAccountID   int64 `json:"to_account_id"`
+	Amount        int64 `json:"amount"`
+}
+
+type TransferTxResult struct {
+	Transfer    Transfer `json:"transfer"`
+	FromAccount Account  `json:"from_account"`
+	ToAccount   Account  `json:"to_account"`
+	FromEntry   Entry    `json:"from_entry"`
+	ToEntry     Entry    `json:"to_entry`
+}
+
+//cria uma transfer record, adiciona entradas em contas e atualiza balance
+func (store *Store) TranferTx(ctx context.Context, arg TransferTxParams) (TransferTxResult, error) {
+	var result TransferTxResult
+	err := store.execTx(ctx, func(q *Queries) error {
+		var err error
+		result.Transfer, err = q.createTransfer(ctx, CreateAccountParams{
+			FromAccountID: arg.FromAccountID,
+			ToAccountID:   arg.ToAccountID,
+			Amount:        arg.Amount,
+		})
+		return nil
+	})
+	return result, err
+}
